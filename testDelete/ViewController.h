605acb5d4ce293be6669172ca496b2d504de91a9
zhanghui <@ioszhanghui@163.com>
2018-06-06 14:52:40 +0800

    first commit

---
+++ testDelete/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testDelete
+//
+//  Created by zhph on 2018/6/6.
+//  Copyright © 2018年 正和普惠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ testDelete/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  testDelete
+//
+//  Created by zhph on 2018/6/6.
+//  Copyright © 2018年 正和普惠. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
